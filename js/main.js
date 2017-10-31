ac2562eb60b614923e9b8022ebd4789574b5545e
Brian Krainer <bkrainer731@gmail.com>
2017-10-30 21:07:13 -0400

    add the verb conjugation table

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ var App = {
 	/* The current verb being displayed */
 	currentVerb: null,
 
+	/* map the format of the dataset to the appropriate pronoun */
+	pronounMap: {
+		form_1s: "yo",
+		form_2s: "tu",
+		form_3s: "el/ella/usted",
+		form_1p: "nosotros",
+		form_2p: "vosotros",
+		form_3p: "gerund",
+		pastparticiple: "past participle"
+	},
+	
 	/* Function to be run on page load. Parses the verb data from the
 	 * CSV and displays the first verb.
 	 */
@@ -40,10 +51,32 @@ var App = {
 		return verb;
 	},
 
-	/* Takes in a verb object, and handles the logic for displaying this verb. */
+	/* Takes in a verb object, and handles the logic for displaying this verb.
+	 * This includes displaying the infinitive form of the verb, along with its
+	 * english definition. This is followed by a table of the verb's pronouns
+	 * paired with the appropriate conjugations.
+	 */
 	displayVerb: function(verb) {
 		$("#infinitive-form").text(verb.infinitive);
 		$("#definition").text(verb.infinitive_english);
+
+		/* We want to display the mood and the tense that we are currently showing */
+		$('#table-header').text(verb.mood_english + ' ' + verb.tense_english);
+
+		/* Clear the current verb table so we don't keep appending information each time
+		 * a new verb is loaded
+		 */
+		$('#verb-table-body').empty();
+
+		/* Iterate through the list of pronouns/alternate verb forms,
+		 * and create a table row. Then append this row to the verb table
+		 */
+		_.each(_.keys(App.pronounMap), function(key) {
+			var $row = $('<tr></tr>');
+			$row.append('<td>' + App.pronounMap[key] + '</td>');
+			$row.append('<td>' + verb[key] + '</td>');
+			$("#verb-table-body").append($row);
+		});
 	},
 
 	/* Handles the clicking of the 'Next' button. Calls getNextVerb and displayVerb. */
